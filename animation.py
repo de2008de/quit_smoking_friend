9adc26460c70e5d21681945a279d357db8b199cb
Yi Ching (David) Chou <mrycchou@gmail.com>
2022-08-21 17:55:21 -0700

    Set size of image

--- animation.py
+++ animation.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 
+from PIL import Image, ImageTk
 
-from tkinterdnd2 import DND_FILES
-
+WIDTH = 150
+HEIGHT = 200
 
 class Animation(tk.Label):
     def __init__(self, *args, **kwargs):
         self.sprite_path = "assets\\sprites"
         self.sprite = "stand1.png"
-        self.image = tk.PhotoImage(file=f"{self.sprite_path}\\{self.sprite}")
+        img = Image.open(f"{self.sprite_path}\\{self.sprite}").resize((WIDTH, HEIGHT))
+        # self.image = tk.PhotoImage(file=f"{self.sprite_path}\\{self.sprite}")
+        self.image = ImageTk.PhotoImage(img)
         super().__init__(*args, **kwargs, image=self.image)
