26f0109a60b109690d52a9d965ddf8cd93469c37
Yi Ching (David) Chou <mrycchou@gmail.com>
2022-08-21 18:50:26 -0700

    Add jump event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
 import tkinterdnd2.TkinterDnD as tkdnd
-import tkinter as tk
 
 
 from animation import WIDTH, HEIGHT
@@ -8,29 +8,51 @@ from PIL import Image, ImageTk
 
 
 SPRITE_PATH = "assets\\sprites"
+_event_number = 0
 _cycle = 0
-_interval = 1000
 
 window = tkdnd.Tk()
 
+
+# Events
 IDLE = []
 for i in range(1, 3):
     img = Image.open(SPRITE_PATH + "\\stand{}.png".format(i)).resize((WIDTH, HEIGHT))
     image = ImageTk.PhotoImage(img)
     IDLE.append(image)
 
+JUMP = []
+for i in range(1, 5):
+    img = Image.open(SPRITE_PATH + "\\jump_face_left_{}.png".format(i)).resize((WIDTH, HEIGHT))
+    image = ImageTk.PhotoImage(img)
+    JUMP.append(image)
 
-def update_animation(cycle, floater):
-    floater.label.configure(image=IDLE[cycle])  # Update the image
-    cycle = (cycle + 1) % len(IDLE)
-    window.after(_interval, update_animation, cycle, floater)
+
+EVENTS = [IDLE, JUMP]
+INTERVALS = [500, 100]
+
+
+def update_animation(cycle, event_number, floater):
+    event = EVENTS[event_number]
+    floater.label.configure(image=event[cycle])  # Update the image
+    cycle = (cycle + 1) % len(event)
+
+    # If event finishes, get next random event
+    if cycle == 0:
+        event_number = get_random_event_number()
+
+    window.after(INTERVALS[event_number], update_animation, cycle, event_number, floater)
+
+
+def get_random_event_number():
+    return random.randint(0, len(EVENTS) - 1)
 
 
 def main():
     window.overrideredirect(True)
     window.wm_attributes("-transparentcolor", "#f0f0f0")
     window.floater = FloatingWindow(window)
-    window.after(_interval, update_animation, _cycle, window.floater)
+    window.after(INTERVALS[_event_number], update_animation, _cycle, _event_number, window.floater)
     window.mainloop()
 
 
