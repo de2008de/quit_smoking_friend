89bc2faa3a6a16afac3381a9ff4ed61e179f35e9
Yi Ching (David) Chou <mrycchou@gmail.com>
2022-08-21 18:25:11 -0700

    Animate sprites

--- animation.py
+++ animation.py
@@ -5,12 +5,11 @@ from PIL import Image, ImageTk
 WIDTH = 150
 HEIGHT = 200
 
+SPRITE_PATH = "assets\\sprites"
 
 class Animation(tk.Label):
     def __init__(self, *args, **kwargs):
-        self.sprite_path = "assets\\sprites"
         self.sprite = "stand1.png"
-        img = Image.open(f"{self.sprite_path}\\{self.sprite}").resize((WIDTH, HEIGHT))
-        # self.image = tk.PhotoImage(file=f"{self.sprite_path}\\{self.sprite}")
+        img = Image.open(f"{SPRITE_PATH}\\{self.sprite}").resize((WIDTH, HEIGHT))
         self.image = ImageTk.PhotoImage(img)
         super().__init__(*args, **kwargs, image=self.image)
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-import tkinterdnd2.TkinterDnD as tk
+import tkinterdnd2.TkinterDnD as tkdnd
+import tkinter as tk
 
+
+from animation import WIDTH, HEIGHT
 from floating_window import FloatingWindow
+from PIL import Image, ImageTk
 
 
-window = tk.Tk()
-window.overrideredirect(True)
-window.wm_attributes("-transparentcolor", "#f0f0f0")
-window.floater = FloatingWindow(window)
-window.mainloop()
+SPRITE_PATH = "assets\\sprites"
+_cycle = 0
+_interval = 1000
+
+window = tkdnd.Tk()
+
+IDLE = []
+for i in range(1, 3):
+    img = Image.open(SPRITE_PATH + "\\stand{}.png".format(i)).resize((WIDTH, HEIGHT))
+    image = ImageTk.PhotoImage(img)
+    IDLE.append(image)
+
+
+def update_animation(cycle, floater):
+    floater.label.configure(image=IDLE[cycle])  # Update the image
+    cycle = (cycle + 1) % len(IDLE)
+    window.after(_interval, update_animation, cycle, floater)
+
+
+def main():
+    window.overrideredirect(True)
+    window.wm_attributes("-transparentcolor", "#f0f0f0")
+    window.floater = FloatingWindow(window)
+    window.after(_interval, update_animation, _cycle, window.floater)
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
