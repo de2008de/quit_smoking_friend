b043d624d6402c25c13aebf82b60b3e7dcc00f3c
Yi Ching (David) Chou <mrycchou@gmail.com>
2022-08-22 09:48:47 -0700

    Load and pick jokes

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from os.path import exists
@@ -12,11 +13,21 @@ FILES = [HOLD_BACK_CIGARETTE_FILE]
 class DataManager:
     def __init__(self):
         self.init_files()
+        self.jokes = self.load_jokes()
 
     def record_hold_back_cigarette(self):
         current_time = time.time()
         with open(DATA_FOLDER + HOLD_BACK_CIGARETTE_FILE, 'a') as f:
             f.write(str(current_time) + '\n')
+
+    def random_joke(self):
+        idx = random.randint(0, len(self.jokes) - 1)
+        return self.jokes[idx]
+
+    def load_jokes(self):
+        with open('assets/jokes/one_liner_jokes.txt', 'r') as f:
+            jokes = f.readlines()
+        return jokes
 
     def init_files(self):
         for file in FILES:
